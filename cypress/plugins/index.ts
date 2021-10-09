f7665c1147321aa7c0229821da72b70db075f171
Ben Wainwright <ben.wainwright@cinch.co.uk>
2021-10-09 15:27:44 +0100

    build: fix linting

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -4,13 +4,9 @@ import { rmdir } from "fs";
 const plugins = (on, config) => {
   on("task", {
     deleteFolder(folderName: string) {
-      console.log("deleting folder %s", folderName);
-
       return new Promise((resolve, reject) => {
         rmdir(folderName, { maxRetries: 10, recursive: true }, err => {
           if (err) {
-            console.error(err);
-
             return reject(err);
           }
 
